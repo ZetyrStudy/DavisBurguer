80cfcfa2b03c34fd365e691538bce8d5f9d34e18
AndersonMoura <74934630+Anderson0312@users.noreply.github.com>
2023-03-13 21:16:08 -0300

    backend categoria

--- src/app/pages/categorias/categorias.page.ts
+++ src/app/pages/categorias/categorias.page.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Produto } from 'src/assets/database/db-produto.interface';
 
 @Component({
   selector: 'app-categorias',
@@ -8,13 +10,31 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class CategoriasPage implements OnInit {
 
-  constructor(public router:ActivatedRoute) { 
+	produto: Produto[] | any;
+  categoriaId: string | any;
+
+  constructor(public router:ActivatedRoute, private http: HttpClient) { 
     this.router.paramMap.subscribe((params:ParamMap)=>{
-      const locail = (params.get('id'))
+      this.categoriaId = (params.get('id'))
+      console.log(params.get('id'))
     })
   }
 
+  featuredSlideOpts = {
+		slidesPerView: 1.2,
+		spaceBetween: 10,
+		freeMode: true
+	};
+
   ngOnInit() {
+    this.http.get('../../../assets/database/db-produto.json')
+    .subscribe((res: any) => {
+      this.produto = res.produto;
+    });
   }
+
+  openPage = function (input: any) {
+		window.location.href = '../detalhes/' + input;
+	};
 
 }
